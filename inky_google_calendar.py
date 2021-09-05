1ba92649bfbade65dd8c542c255f0188ac00860a
Bonce <chris@headstrong.solutions>
2021-09-05 12:32:58 +0100

    Corrcted colours to best saturated options

--- inky_google_calendar.py
+++ inky_google_calendar.py
@@ -4,11 +4,10 @@ import os
 import time
 from datetime import datetime 
 from typing import List
-from inky import inky_uc8159
 
-#from inky.inky_uc8159 import Inky
+from inky.inky_uc8159 import Inky
 # To simulate:
-from inky.mock import InkyMockImpression as Inky
+#from inky.mock import InkyMockImpression as Inky
 from inky.auto import auto
 
 from font_source_sans_pro import SourceSansProLight
@@ -24,13 +23,13 @@ light_font = ImageFont.truetype(SourceSansProLight, 14)
 normal_font = ImageFont.truetype(SourceSansPro, 14)
 semibold_font = ImageFont.truetype(SourceSansProSemibold, 14)
 
-BLACK=(57, 48, 57)
+BLACK=(0,0,0)
 WHITE=(255, 255, 255)
-GREEN=(58, 91, 70)
-BLUE=(61, 59, 94)
-RED=(156, 72, 75)
-YELLOW=(208, 190, 71)
-ORANGE=(177, 106, 73)
+GREEN=(0, 255, 0)
+BLUE=(0, 0, 255)
+RED=(255, 0, 0)
+YELLOW=(255, 255, 0)
+ORANGE=(255, 140, 0)
 CLEAR=(255, 255, 255)
 
 PATH = os.path.dirname(__file__)
@@ -39,25 +38,25 @@ img = Image.open(os.path.join(PATH, "resources/backdrop.png")).resize(inky.resol
 draw = ImageDraw.Draw(img)
 
 def draw_day_headers(box_width:int):
-    draw.rectangle((1, 1, 600, 20), fill=inky.ORANGE)
-    draw.text(((box_width*0)+30,2), "MON" , inky.WHITE, font=semibold_font)
-    draw.text(((box_width*1)+30,2), "TUE" , inky.WHITE, font=semibold_font)
-    draw.text(((box_width*2)+30,2), "WED" , inky.WHITE, font=semibold_font)
-    draw.text(((box_width*3)+30,2), "THU" , inky.WHITE, font=semibold_font)
-    draw.text(((box_width*4)+30,2), "FRI" , inky.WHITE, font=semibold_font)
-    draw.text(((box_width*5)+30,2), "SAT" , inky.WHITE, font=semibold_font)
-    draw.text(((box_width*6)+30,2), "SUN" , inky.WHITE, font=semibold_font)
+    draw.rectangle((1, 1, 600, 20), fill=BLUE)
+    draw.text(((box_width*0)+30,2), "MON" , WHITE, font=semibold_font)
+    draw.text(((box_width*1)+30,2), "TUE" , WHITE, font=semibold_font)
+    draw.text(((box_width*2)+30,2), "WED" , WHITE, font=semibold_font)
+    draw.text(((box_width*3)+30,2), "THU" , WHITE, font=semibold_font)
+    draw.text(((box_width*4)+30,2), "FRI" , WHITE, font=semibold_font)
+    draw.text(((box_width*5)+30,2), "SAT" , WHITE, font=semibold_font)
+    draw.text(((box_width*6)+30,2), "SUN" , WHITE, font=semibold_font)
 
 def draw_day(x:int, y:int, box_width:int, box_height:int, today_string:str, events:List[str], today=False):
     date_box_height = 15
     # draw date background
-    colour = inky.YELLOW
+    colour = RED
     if today:
-        colour = inky.GREEN
+        colour = GREEN
     draw.rectangle((x, y, x+box_width, y+date_box_height), 
                     fill=colour)
-    draw.rectangle((x, y+date_box_height, x+box_width, y+box_height+date_box_height-1), 
-                    fill=inky.WHITE)
+    draw.rectangle((x, y+date_box_height, x+box_width, y+box_height), 
+                    fill=WHITE)
 
     # write date
     center_start_pos = 0
@@ -67,7 +66,7 @@ def draw_day(x:int, y:int, box_width:int, box_height:int, today_string:str, even
         center_start_pos = 27
 
     # date header
-    draw.text((x + center_start_pos, y-2), today_string , inky.WHITE, font=semibold_font)
+    draw.text((x + center_start_pos, y-2), today_string , WHITE, font=semibold_font)
 
     # general y offset
     offset_y = y + (date_box_height -2)
@@ -83,8 +82,9 @@ def draw_day(x:int, y:int, box_width:int, box_height:int, today_string:str, even
                 if event.end:
                     friendly_time_end = event.end.strftime("%H:%M")
                     friendly_time = str("%s - %s" % (friendly_time, friendly_time_end))
-                draw.text((event_x, event_y), friendly_time , inky.BLUE, font=light_font)
-                draw.text((event_x, event_y+13), event.title[:13] , inky.BLACK, font=normal_font)
+                    draw.text((event_x, event_y), event.title[:13] , BLUE, font=normal_font)
+                    draw.text((event_x, event_y+13), friendly_time , GREEN, font=normal_font)
+                
                 event_y = event_y + 30
                 event_count = event_count +1
 
@@ -126,12 +126,11 @@ for day in calendar_data.events.dates:
         y = y + box_height + 15
 
 # right and bottom edges
-draw.line((598,0,598,446), inky.YELLOW)
-draw.line((0,446,598,446), inky.YELLOW)
+draw.line((598,0,598,446), RED)
+draw.line((0,446,598,446), RED)
 
-inky.set_image(img)
+inky.set_image(img, saturation=1)
 inky.show()
 # To simulate:
-inky.wait_for_window_close()
-
+#inky.wait_for_window_close()
 
